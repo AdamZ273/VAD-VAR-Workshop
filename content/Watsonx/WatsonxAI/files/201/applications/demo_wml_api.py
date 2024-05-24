6ce81860b1f04787f59355aa6be275085ac65eb9
AdamZ273 <161613331+AdamZ273@users.noreply.github.com>
2024-05-24 16:52:16 +0700

    Update demo_wml_api.py

--- content/Watsonx/WatsonxAI/files/201/applications/demo_wml_api.py
+++ content/Watsonx/WatsonxAI/files/201/applications/demo_wml_api.py
@@ -35,8 +35,8 @@ from ibm_cloud_sdk_core import IAMTokenManager
 # URL of the hosted LLMs is hardcoded because at this time all LLMs share the same endpoint
 url = "https://us-south.ml.cloud.ibm.com"
 
-# These global variables will be updated in get_credentials() function
-watsonx_project_id = "41b857f4-2b79-44fd-a599-e4aef3801293"
+# These global variables will be updated in get_credentials() functions
+watsonx_project_id = "802e1ce3-1bd8-45fb-a943-3bae2c8d7b17"
 # Replace with your IBM Cloud key
 api_key = "Yx0r5Cf7eMtxHZMDtS1Ud520eW0KNdNOtJG8Kg5dUXLL"
 
